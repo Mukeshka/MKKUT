91a6104e855f45006a0024c1366a0403b6823009
Mukeshka <86514276+Mukeshka@users.noreply.github.com>
2021-09-11 13:35:26 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ apt update
 pkg install git
 pkg install python 
 pkg install figlet
-rm 
-wget -q https://github.com/Mukeshka/MKKUT/blob/main/PYTHON.PY
+
+wget -q https://github.com/Mukeshka/MKKUT/blob/main/run.PY
 clear
-python PYTHON.PY
+python run.PY
