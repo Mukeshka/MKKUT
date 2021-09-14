c231f1196a0426f948e9c8b5c76716806d52a7dc
Mukeshka <86514276+Mukeshka@users.noreply.github.com>
2021-09-14 13:31:21 +0530

    Life.py

--- life.py
+++ life.py
@@ -130,7 +130,7 @@ def hero():
 
 
 
-if(expirydate>today and playday==today):
+if(expirydate>today):
     now = datetime.datetime.now()
     First = now.replace(hour=10, minute=55, second=0, microsecond=0)
     Firstend = now.replace(hour=11, minute=35, second=0, microsecond=0)
