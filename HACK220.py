5fa7a2af0a8fa109e2adf07aca9bb64efca58951
Mukeshka <86514276+Mukeshka@users.noreply.github.com>
2021-12-29 16:42:13 +0530

    Update HACK220.py

--- HACK220.py
+++ HACK220.py
@@ -89,7 +89,7 @@ def hero():
          if i in thisway:
             m=getSum(current)
             n=int(current)%10
-            if((m%2==-1 and n%2==-1) or (m%2==1 and n%2==1)):
+            if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
                 if current in numbers:
                     print(newperiod+1," : 💥🟢GREEN🟢💥")
                 else:
