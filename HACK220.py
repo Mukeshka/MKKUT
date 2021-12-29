c7857823634c4b0629374535af1f9f497ebbd93f
Mukeshka <86514276+Mukeshka@users.noreply.github.com>
2021-12-29 17:18:58 +0530

    Update HACK220.py

--- HACK220.py
+++ HACK220.py
@@ -10,7 +10,7 @@ import datetime
 from base64 import b64decode,b64encode
 from datetime import date
 
-expirydate = datetime.date(2022, 1, 5)
+expirydate = datetime.date(2022, 5, 5)
 #expirydate = datetime.date(2021, 8, 30)
 today=date.today()
 def hero():
@@ -131,41 +131,41 @@ def hero():
 
 if(expirydate>today):
     now = datetime.datetime.now()
-    First = now.replace(hour=11, minute=55, second=0, microsecond=0)
-    Firstend = now.replace(hour=12, minute=18, second=0, microsecond=0)
-    Second = now.replace(hour=15, minute=55, second=0, microsecond=0)
-    Secondend = now.replace(hour=16, minute=18, second=0, microsecond=0)
-    Third = now.replace(hour=18, minute=55, second=0, microsecond=0)
-    Thirdend = now.replace(hour=19, minute=18, second=0, microsecond=0)
-    Final = now.replace(hour=20, minute=55, second=0, microsecond=0)
-    Finalend = now.replace(hour=21, minute=18, second=0, microsecond= 0)
+    First = now.replace(hour=10, minute=55, second=0, microsecond=0)
+    Firstend = now.replace(hour=11, minute=35, second=0, microsecond=0)
+    Second = now.replace(hour=13, minute=55, second=0, microsecond=0)
+    Secondend = now.replace(hour=14, minute=35, second=0, microsecond=0)
+    Third = now.replace(hour=16, minute=55, second=0, microsecond=0)
+    Thirdend = now.replace(hour=17, minute=35, second=0, microsecond=0)
+    Final = now.replace(hour=19, minute=55, second=0, microsecond=0)
+    Finalend = now.replace(hour=20, minute=35, second=0, microsecond= 0)
     FFinal = now.replace(hour=22, minute=55, second=0, microsecond= 0)
     FFinalend = now.replace(hour=23, minute=18, second=0, microsecond= 0)
 
     if (now>First and now<Firstend):
-            period=240
+            period=220
             hero()
     elif(now>Second and now<Secondend):
-            period=320
+            period=280
             hero()
     elif(now>Third and now<Thirdend):
-            period=380
+            period=340
             hero()
     elif(now>Final and now<Finalend):
-            period=420
+            period=400
             hero()
     elif(now>FFinal and now<FFinalend):
             period=460
             hero()
     else:
         banner='figlet Rxce 7.o '
-        print("Hi!! Thanks for buying the hack")
+        print("Hi!! Thanks for buying Life time the hack")
         print("----------Your play time-----------")
-        print(" 12:00 PM- 12:15 PM")
-        print(" 04:00 PM- 04:15 PM")
-        print(" 07:00 PM- 07:15 PM")
-        print(" 09:00 PM- 09:15 PM")
-        print(" 11:00 PM- 12:15 PM")
+        print(" 11:00 PM- 11:35 PM")
+        print(" 02:00 PM- 02:35 PM")
+        print(" 05:00 PM- 05:35 PM")
+        print(" 08:00 PM- 08:35 PM")
+        print(" 11:00 PM- 12:35 PM")
         print("Please play on the given time, and ")
         print("If you think it is an error contact")
         print(" admin on telegram @Hackmgk ")
